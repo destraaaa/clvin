1da73e8a40176d7a5ed34634764f63189aa2e88f
aryl000 <aryl.otniel@tokopedia.com>
2018-04-16 17:45:47 +0700

    connect db and use gin

--- app.go
+++ app.go
@@ -1,45 +1,43 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"net/http"
+
 	// "encoding/json"
-	"github.com/google/gops/agent"
-	// "github.com/gosample/interviewee"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	// "github.com/tokopedia/gosample/hello"
-	"github.com/gosample/hello"
-	"github.com/tokopedia/logging/tracer"
-	"gopkg.in/tokopedia/grace.v1"
-	"gopkg.in/tokopedia/logging.v1"
+	"github.com/gin-gonic/gin"
+	"github.com/gosample/interviewee"
 )
 
 func main() {
+	r := gin.Default()
 
-	flag.Parse()
-	logging.LogInit()
+	r.POST("/interviewee/save", interviewee.WriteData)
+	r.Run()
 
-	debug := logging.Debug.Println
+	// flag.Parse()
+	// logging.LogInit()
 
-	debug("app started") // message will not appear unless run with -debug switch
+	// debug := logging.Debug.Println
 
-	if err := agent.Listen(&agent.Options{}); err != nil {
-		log.Fatal(err)
-	}
+	// debug("app started") // message will not appear unless run with -debug switch
 
-	hwm := hello.NewHelloWorldModule()
-	// interview := interviewee.WriteData()
+	// if err := agent.Listen(&agent.Options{}); err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	http.Handle("/metrics", promhttp.Handler())
+	// hwm := hello.NewHelloWorldModule()
+	// // interview := interviewee.WriteData()
 
-	http.HandleFunc("/hello", hwm.SayHelloWorld)
-	go logging.StatsLog()
+	// http.Handle("/metrics", promhttp.Handler())
 
-	// http.HandleFunc("/interviewee/save", interview.WriteJson)
+	// http.HandleFunc("/hello", hwm.SayHelloWorld)
 	// go logging.StatsLog()
 
-	tracer.Init(&tracer.Config{Port: 8700, Enabled: true})
+	// //http.HandleFunc("/interviewee/save", interviewee.WriteData)
+	// // go logging.StatsLog()
 
-	log.Fatal(grace.Serve(":9000", nil))
+	// tracer.Init(&tracer.Config{Port: 8700, Enabled: true})
+
+	// log.Fatal(grace.Serve(":9000", nil))
 }
--- interviewee/init.go
+++ interviewee/init.go
@@ -2,16 +2,33 @@ package interviewee
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	// "github.com/lib/pq"
+
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/lib/pq"
+	//_ "github.com/lib/pq"
 )
 
 type NonOps struct {
-	id     int
-	email  string
-	status bool
+	Fid               int
+	FfullName         string `json:"fullName"`
+	FnickName         string `json:"nickName"`
+	FphoneNumber      string `json:"phoneNumber"`
+	Femail            string `json:"email"`
+	Fschool           string `json:"school"`
+	Fmajor            string `json:"major"`
+	FGPA              string `json:"GPA"`
+	Fpurpose          string `json:"purpose"`
+	Fmeet             string `json:"meet"`
+	Fposition         string `json:"position"`
+	Ftime             string `json:"time"`
+	FinfoJob          string `json:"infoJob"`
+	Facquaintance     string `json:"acquaintance"`
+	FacquaintanceName string `json:"acquaintanceName"`
+	Frelationship     string `json:"relationship"`
+	FreferralName     string `json:"referralName"`
+	Fstatus           bool
 }
 
 const (
@@ -19,10 +36,32 @@ const (
 	port     = 5432
 	user     = "postgres"
 	password = "newpassword"
-	dbname   = "Hris"
+	dbname   = "hris"
 )
 
-func WriteData() {
+func WriteData(c *gin.Context) {
+	// fmt.Printf([]byte(r.Body))
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	// var data NonOps
+	// c.BindJSON(&data)
+	// fmt.Println(data)
+
+	// w.Header().Set("Content-Type", "application/json")
+	// w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+	// w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	// fmt.Printf("%s", bodyBuffer)
+	//json.NewEncoder(w).Encode(bodyBuffer)
+	var nonops NonOps
+	err := c.BindJSON(&nonops)
+	if err != nil {
+		fmt.Println("Error JSON Unmarshal")
+		fmt.Println(err.Error())
+	}
+
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
@@ -40,62 +79,66 @@ func WriteData() {
 	fmt.Println("Successfully connected!")
 
 	sqlStatement := `
-		INSERT INTO NonOps (fullname, nickname,
-			email, phone, school, major, gpa, purpose, contactPersonId,
-			positionApply, jobInfo, acquintances, scheduleTime,
-			acquintancesName, relationship, referralName, status)
-		VALUES ("aryl man","aryl", "arylman@gmail.com","081212121212",
-			"BINUS", "IT", "3.18", "Interview with User",
-			"Ms.Clarissa", "Software Engineering", "Job Fair","YES",
-			"10:30PM", "Lala", "Brother/Sister", "Mitha", true
-			)
-		RETURNING email`
-
-	email := ""
-	err = db.QueryRow(sqlStatement, "aryl man", "aryl", "arylman@gmail.com", "081212121212",
-		"BINUS", "IT", "3.18", "Interview with User",
-		"Ms.Clarissa", "Software Engineering", "Job Fair", "YES",
-		"10:30PM", "Lala", "Brother/Sister", "Mitha", true).Scan(&email)
-
+				INSERT INTO NonOps (fullname, nickname,
+					email, phone, school, major, gpa, purpose, contactPersonId,
+					positionApply, jobInfo, acquintances, scheduleTime,
+					acquintancesName, relationship, referralName, status)
+				VALUES ($1,$2, $3,$4,
+					$5, $6, $7, $8,
+					$9, $10, $11,$12,
+					$13, $14, $15, $16, true
+					)`
+	_, err = db.Exec(sqlStatement, nonops.FfullName, nonops.FnickName, nonops.Femail,
+		nonops.FphoneNumber, nonops.Fschool, nonops.Fmajor, nonops.FGPA, nonops.Fpurpose,
+		nonops.Fmeet, nonops.Fposition, nonops.FinfoJob,
+		nonops.Facquaintance, nonops.Ftime, nonops.FacquaintanceName,
+		nonops.Frelationship, nonops.FreferralName)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("New record email is:", email)
+	//fmt.Printf("hello %s, you is %s, your email is %s \n", nonops.FfullName, nonops.FnickName, nonops.Femail)
+
+	c.Writer.Write([]byte(nonops.FacquaintanceName))
+
 }
 
-func WriteJson(w http.ResponseWriter, r *http.Request) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+// func WriteData() {
+// 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+// 		"password=%s dbname=%s sslmode=disable",
+// 		host, port, user, password, dbname)
+// 	db, err := sql.Open("postgres", psqlInfo)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer db.Close()
 
-	rows, err := db.Query("SELECT id, email, status FROM NonOps ORDER BY timestamp DESC LIMIT 1")
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var email string
-		var status bool
-		err = rows.Scan(&id, &email, &status)
-		if err != nil {
-			panic(err)
-		}
+// 	err = db.Ping()
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-		NonOps := NonOps{id, email, status}
-		js, err := json.Marshal(NonOps)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
-	}
+// 	fmt.Println("Successfully connected!")
 
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	}
+// 	sqlStatement := `
+// 		INSERT INTO NonOps (fullname, nickname,
+// 			email, phone, school, major, gpa, purpose, contactPersonId,
+// 			positionApply, jobInfo, acquintances, scheduleTime,
+// 			acquintancesName, relationship, referralName, status)
+// 		VALUES ("aryl man","aryl", "arylman@gmail.com","081212121212",
+// 			"BINUS", "IT", "3.18", "Interview with User",
+// 			"Ms.Clarissa", "Software Engineering", "Job Fair","YES",
+// 			"10:30PM", "Lala", "Brother/Sister", "Mitha", true
+// 			)
+// 		RETURNING email`
 
-}
+// 	email := ""
+// 	err = db.QueryRow(sqlStatement, "aryl man", "aryl", "arylman@gmail.com", "081212121212",
+// 		"BINUS", "IT", "3.18", "Interview with User",
+// 		"Ms.Clarissa", "Software Engineering", "Job Fair", "YES",
+// 		"10:30PM", "Lala", "Brother/Sister", "Mitha", true).Scan(&email)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("New record email is:", email)
+// }
